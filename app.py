37c0ae9f514cbcfb15106956a2a416fd741d4ee3
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2024-12-26 14:46:21 +0530

    improved

--- app.py
+++ app.py
@@ -154,9 +154,9 @@ if __name__ == '__main__':
 if __name__ == "__main__":
     app.run()pip freeze > requirements.txt'''
 
-"""
+
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
-    print("active")"""
+    print("active")
 
